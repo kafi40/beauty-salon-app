46891d6c7244e0b0423434cb1b4beb7813b11bf1
Fairgraves <rozaco0@gmail.com>
2024-12-08 18:32:49 +0700

    Изменить тип меню

--- beauty-salon-bot/beauty-salon-bot-handler/src/main/java/ru/kafi/beautysalonbothandler/factory/ButtonFactory.java
+++ beauty-salon-bot/beauty-salon-bot-handler/src/main/java/ru/kafi/beautysalonbothandler/factory/ButtonFactory.java
@@ -1,26 +1,72 @@
 package ru.kafi.beautysalonbothandler.factory;
 
+import org.checkerframework.checker.units.qual.A;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ButtonFactory {
 
-    public static ReplyKeyboard getMainMenuKeyBoard() {
-        KeyboardRow row1 = new KeyboardRow();
-        row1.add("Прайс-лист");
-        row1.add("Подписаться");
-        row1.add("Галерея");
-        KeyboardRow row2 = new KeyboardRow();
-        row2.add("Записаться");
-        row2.add("Зарегистрировать аккаунт");
-        row2.add("Вывести мастеров");
-        KeyboardRow row3 = new KeyboardRow();
-        row3.add("Завершить сеанс");
-        row3.add("Контакты\\связаться");
-        return new ReplyKeyboardMarkup(List.of(row1,row2,row3));
+    public static InlineKeyboardMarkup getMainMenuKeyBoard() {
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        InlineKeyboardButton button;
+
+        button = new InlineKeyboardButton();
+        button.setText("Прайс-лист");
+        button.setCallbackData("price-list");
+        row.add(button);
+
+        button = new InlineKeyboardButton();
+        button.setText("Подписаться");
+        button.setCallbackData("subscribe");
+        row.add(button);
+
+        button = new InlineKeyboardButton();
+        button.setText("Галерея");
+        button.setCallbackData("galery");
+        row.add(button);
+
+        rows.add(row);
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton();
+        button.setText("Записаться");
+        button.setCallbackData("appointment");
+        row.add(button);
+
+        button = new InlineKeyboardButton();
+        button.setText("Вывести мастеров");
+        button.setCallbackData("masters");
+        row.add(button);
+
+        button = new InlineKeyboardButton();
+        button.setText("Зарегистрировать аккаунт");
+        button.setCallbackData("register");
+        row.add(button);
+
+        rows.add(row);
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton();
+        button.setText("Завершить сеанс");
+        button.setCallbackData("stop");
+        row.add(button);
+
+        button = new InlineKeyboardButton();
+        button.setText("Контакты\\связаться");
+        button.setCallbackData("info");
+        row.add(button);
+
+        rows.add(row);
+
+        return new InlineKeyboardMarkup(rows);
 
     }
 }
